d8646220f2fa30b3d9e93c37999beef30095ce9a
Ross <rossrtw2010@gmail.com>
2018-04-09 19:16:25 +0700

    HTML variables updated via DOM.

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
 // Selectors
-const bckgrndImg = document.querySelector('body')
-const currLoc = document.querySelector('.header#location')
-const pictogram = document.querySelector('#picto')
-const adj = document.querySelector('.text#adjective')
-const temperature = document.querySelector('.text#temperature')
-const forecast = document.querySelector('.text#description')
+let bckgrndImg = document.querySelector('body')
+let currLoc = document.querySelector('#location')
+let pictogram = document.querySelector('#picto')
+let adj = document.querySelector('#adjective')
+let temperature = document.querySelector('#temperature')
+let forecast = document.querySelector('#description')
 
+let weather = ''
 let longitude = -4.7, latitude = 55.9
 
 //Get request
 function getWeather() {
-  let url = 'https://fcc-weather-api.glitch.me/api/current?lat=55.9&lon=-4.7';
+  let url = 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139'
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.onload = function() {
@@ -19,8 +20,39 @@ function getWeather() {
     } else {
       alert('Request failed.  Returned status of ' + xhr.status);
     }
+    // Make DOM changes
+    currLoc.textContent = weather.name;
+    temperature.textContent = Math.round(weather.main.temp);
+    switch (true) {
+    case (weather.main.temp <= 0):
+      adj.textContent = 'FREEZING'
+      break;
+    case (weather.main.temp <= 7):
+      adj.textContent = 'cold'
+      break;
+    case (weather.main.temp <= 12):
+      adj.textContent = 'chilly'
+      break;
+    case (weather.main.temp <= 17):
+      adj.textContent = 'pleasant'
+      break;
+    case (weather.main.temp <= 23):
+      adj.textContent = 'nice'
+      break;
+    case (weather.main.temp <= 28):
+      adj.textContent = 'warm'
+      break;
+    case (weather.main.temp <= 35):
+      adj.textContent = 'hot'
+      break;
+    case (weather.main.temp > 35):
+      adj.textContent = 'SIZZLING'
+      break;
+    }
+    forecast.textContent = weather.weather[0].description;
+    pictogram.src = weather.weather[0].icon;
   };
   xhr.send();
 }
 
-getWeather()
+document.addEventListener('DOMContentLoaded', getWeather());
